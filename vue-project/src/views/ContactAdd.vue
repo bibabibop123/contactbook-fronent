a831a80935eb731c6fccf2372d6c9f10b11adc1c
bibabibop123 <thaison13021998@gmail.com>
2023-04-06 09:48:55 +0700

    tao chuc nang them moi lien he

---
+++ vue-project/src/views/ContactAdd.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" @delete:contact="deleteContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {},
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
